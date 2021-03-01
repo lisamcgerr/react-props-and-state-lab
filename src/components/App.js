b0a7830db328a39fb69e391e400ea0e8758b3723
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-02-28 18:07:34 -0600

    commit tree f75df1c72fdbc7767a03f47aff3cef62f5774f6f

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,83 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (e) => {
+    this.setState({
+      filters: {
+        type: e.target.value
+      }
+    })
+  }
+
+  onFindPetsClick = () => {
+     //console.log('pets click')
+     //use console.log to check if the onClick is being rendered
+
+    //due to scope cannot be accessed in the fetch(url)
+    //needs to be defined
+
+    let url = this.state.filters.type === 'all' ? '/api/pets' : `/api/pets?type=${this.state.filters.type}`
+
+    //option 2
+    // let url;
+    // if (this.state.filters.type === 'all'){
+    //   url = '/api/pets'
+    // } else {
+    //   url = `/api/pets?type=${this.state.filters.type}`
+    // }
+
+
+    //option 3
+    // let url;
+    //due to scope cannot be accessed in the fetch(url)
+    //needs to be defined
+
+    // if (this.state.filters.type === 'all'){
+    //   url = '/api/pets'
+    // }
+    // else if (this.state.filters.type === 'cat'){
+    //   url = '/api/pets?type=cat'
+    // }
+    // else if (this.state.filters.type === 'dog'){
+    //   url = '/api/pets?type=dog'
+    // }
+    // else if (this.state.filters.type === 'micropig'){
+    //   url = '/api/pets?type=micropig'
+    // }
+    
+
+
+    //console.log('pets click')
+    fetch(url)
+    .then(resp => resp.json())
+    .then(petsArr => {
+      //updating the state of the pets array  
+      this.setState({
+        pets: petsArr
+      })
+      //console.log(data)
+      //click on the find pets button to grab the data (api)
+    })
+  }
+
+  onAdoptPet = (id) => {
+    //console.log('pet adopted')
+    const updatedPets = this.state.pets.map(petObj => {
+      if (id === petObj.id){
+        return {
+          ...petObj,
+          isAdopted: true
+        }
+      } else {
+        return petObj
+      }
+    })
+
+    this.setState({
+      pets: updatedPets
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +101,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
